12af8af07919fbd64e88d6a729cc3aa4fcc76231
Guilherme China <echina725@gmail.com>
2024-05-21 12:55:31 -0300

    adds format specifier content

--- src/main.c
+++ src/main.c
@@ -2,7 +2,34 @@
 #include <stdlib.h>
 
 // the entry point of the program
+/*
+ * multiline
+ * comment
+ *
+ */
+
+void format_string();
+void isoceles();
+
 int main() {
-  puts("Hello world1");
-  return EXIT_SUCCESS;
+  format_string();
+  // isoceles();
+  return 0;
+}
+
+void format_string() {
+  printf("name: %s\nage:%d\ngender: %s\n", "churros augusto", 9, "male");
+  printf("hello churros\n");
+  printf("I am %d\n", 19);
+  printf("My weight: %.2f\n", 58.659);
+  printf("My weight: %d\n", 58.5); // any memory value in this case
+}
+
+void isoceles() {
+  printf("  * \n ***\n*****\n");
+  printf("------------\n");
+
+  printf("  *\n");
+  printf(" ***\n");
+  printf("*****\n");
 }
